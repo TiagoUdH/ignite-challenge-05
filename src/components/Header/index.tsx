b3951ea1dc2305f80d430ed608071d6fc7dbeca4
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-12-29 14:07:55 -0300

    Creating Header Structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link"
+
 export default function Header() {
-  // TODO
+  return (
+    <header>
+      <figure>
+        <Link href="/">
+          <img src="logo.svg" alt="logo" />
+        </Link>
+      </figure>
+    </header>
+  )
 }
