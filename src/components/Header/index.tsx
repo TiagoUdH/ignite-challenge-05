d28a79e3943e918d521078de62f7b97cddc64adf
Tiago Fernandes <tiagoferprojects@gmail.com>
2023-12-29 14:17:56 -0300

    Creating Header Styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link"
 
+import { useRouter } from "next/router"
+import commonStyles from "../../styles/common.module.scss"
+import styles from "./header.module.scss"
+
 export default function Header() {
+  const { asPath } = useRouter()
+
+  const marginsClassName = asPath === "/" ? styles.margins : ''
+
   return (
-    <header>
-      <figure>
+    <header className={`${commonStyles.container} ${styles.container} ${marginsClassName}`}>
+      <figure className={commonStyles.content}>
         <Link href="/">
           <img src="logo.svg" alt="logo" />
         </Link>
